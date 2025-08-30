11c9d92c81c3ef8050aa44c10d8ccc01b5b92f1f
vijay yadav <124396738+vjyadav12@users.noreply.github.com>
2025-08-31 01:50:05 +0530

    Changements

--- src/App.jsx
+++ src/App.jsx
@@ -5,21 +5,24 @@ import LoginPage from "./Components/Pages/LoginPage";
 import { Routes, Route, useLocation } from "react-router-dom";
 
 function App() {
-  const location = useLocation();
+  // const location = useLocation();
 
   // check if current path is login
-  const hideLayout = location.pathname === "/login";
+  // const hideLayout = location.pathname === "/login";
 
   return (
     <>
-      {!hideLayout && <Header />}
+      {/* {!hideLayout && <Header />} */}
+
+      <Header />
 
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/login" element={<LoginPage />} />
       </Routes>
 
-      {!hideLayout && <Footer />}
+      <Footer />
+      {/* {!hideLayout && <Footer />} */}
     </>
   );
 }
