59a2324722741d68c3ffafb79eaae00ef8044fe1
vijay yadav <124396738+vjyadav12@users.noreply.github.com>
2025-08-30 13:17:10 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import LANDASPage from "./Components/Pages/LANDASPage";
+
+
+
+function App() {
+  return (
+    <>
+      <h1>Hello World</h1>
+      <LANDASPage/>
+    </>
+  );
+}
+
+export default App;
---
+++ src/Components/Pages/LANDASPage.jsx
@@ -0,0 +1,9 @@
+const LANDASPage = ()=>{
+    return(
+        <>
+        <h1>Welcome to Landing page sir</h1>
+        </>
+    )
+}
+
+export default LANDASPage;
